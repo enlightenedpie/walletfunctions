5460d249bf34aab48806cd9c3e82622fe1d15c23
David Paul <enlightenedpie@gmail.com>
2021-01-22 14:46:08 -0600

    pushing functions changes

---
+++ src/provinces.js
@@ -0,0 +1,103 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+var provinces = {
+  US: [
+    'AL',
+    'AK',
+    'AS',
+    'AZ',
+    'AR',
+    'CA',
+    'CO',
+    'CT',
+    'DE',
+    'DC',
+    'FM',
+    'FL',
+    'GA',
+    'GU',
+    'HI',
+    'ID',
+    'IL',
+    'IN',
+    'IA',
+    'KS',
+    'KY',
+    'LA',
+    'ME',
+    'MH',
+    'MD',
+    'MA',
+    'MI',
+    'MN',
+    'MS',
+    'MO',
+    'MT',
+    'NE',
+    'NV',
+    'NH',
+    'NJ',
+    'NM',
+    'NY',
+    'NC',
+    'ND',
+    'MP',
+    'OH',
+    'OK',
+    'OR',
+    'PW',
+    'PA',
+    'PR',
+    'RI',
+    'SC',
+    'SD',
+    'TN',
+    'TX',
+    'UT',
+    'VT',
+    'VI',
+    'VA',
+    'WA',
+    'WV',
+    'WI',
+    'WY',
+  ],
+  CA: [
+    'AB',
+    'BC',
+    'MB',
+    'NB',
+    'NL',
+    'NT',
+    'NS',
+    'NU',
+    'ON',
+    'PE',
+    'QC',
+    'SK',
+    'YT',
+  ],
+};
+
+exports.handler = async function (event, context, callback) {
+  if (event.httpMethod !== 'GET') {
+    return callback(null, {
+      statusCode: 403,
+      body: JSON.stringify({
+        error: 'RESTVerbError',
+        message: 'Only GET requests allowed, you cheeky monkey!!',
+      }),
+    });
+  }
+
+  /* let key = '';
+  if (key in provinces) {
+    return true;
+  } */
+
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: event.path,
+    }),
+  });
+};
