d4e4e8094cabf923b6cfc70e03c697c918e017af
David Paul <enlightenedpie@gmail.com>
2021-04-18 15:26:28 -0500

    update

--- src/transactions.js
+++ src/transactions.js
@@ -2,18 +2,6 @@ const crypto = require("crypto");
 
 var transactionHistory = {};
 
-if (transactionHistory.length < 1) [...30].map((ind) => {
-  let tID = crypto.randomBytes(16).toString(36);
-
-  transactionHistory[tID] = {
-    type: (ind+1 % 2) ? 'withdrawal' : 'deposit',
-    amount: 39900,
-    timestamp: new Date('2019-10-10'),
-    to: 'Jack',
-    from: 'Diane'
-  };
-});
-
 exports.handler = async function (event, context, callback) {
   if (event.httpMethod !== 'GET') {
     return callback(null, {
@@ -24,6 +12,18 @@ exports.handler = async function (event, context, callback) {
       }),
     });
   }
+
+  if (transactionHistory.length < 1) [...30].map((ind) => {
+    let tID = crypto.randomBytes(16).toString(36);
+  
+    transactionHistory[tID] = {
+      type: (ind+1 % 2) ? 'withdrawal' : 'deposit',
+      amount: 39900,
+      timestamp: new Date('2019-10-10'),
+      to: 'Jack',
+      from: 'Diane'
+    };
+  });
 
   const { max = 10, page = 1 } = event.queryStringParameters,
     returnObj = {};
