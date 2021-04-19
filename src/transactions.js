f052399633dc95dbf5017ad8eff7d36fd7e96711
David Paul <enlightenedpie@gmail.com>
2021-04-18 21:42:54 -0500

    update

--- src/transactions.js
+++ src/transactions.js
@@ -1,14 +1,6 @@
 const crypto = require("crypto");
 
-var transactionHistory = {
-  'utfutf-yicviyf3cykugc3': {
-    type: (0+1 % 2) ? 'withdrawal' : 'deposit',
-    amount: 39900,
-    timestamp: new Date('2019-10-10'),
-    to: 'Jack',
-    from: 'Diane'
-  }
-};
+var transactionHistory = {};
 
 exports.handler = async function (event, context, callback) {
   if (event.httpMethod !== 'GET') {
