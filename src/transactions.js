8c4d5124089ed2dabf133493a32cfc8770d99967
David Paul <enlightenedpie@gmail.com>
2021-04-18 22:00:04 -0500

    update

--- src/transactions.js
+++ src/transactions.js
@@ -41,6 +41,6 @@ exports.handler = async function (event, context, callback) {
 
   return callback(null, {
     statusCode: 200,
-    body: JSON.stringify(transactionHistory),
+    body: JSON.stringify(returnObj),
   });
 };
