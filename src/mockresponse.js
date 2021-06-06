c413ac6e4ba0f85e8c017ec78098a60756ac1224
David Paul <enlightenedpie@gmail.com>
2021-06-06 13:04:58 -0500

    push functions

--- src/mockresponse.js
+++ src/mockresponse.js
@@ -2,6 +2,8 @@ exports.handler = async function (event, context, callback) {
 
   let keys = event.path.replace(/\/mockresponse\/?/gm, '').split('/');
 
+  setTimeout(() => {return;}, parseInt(keys[1]) || 0);
+
   return callback(null, {
     statusCode: parseInt(keys[0]) || 200,
     body: event.body,
